5d43f53a5466f952122ccfa77e96bac8fe94e67b
om1ji <AAKhasanbaev@stud.kpfu.ru>
2023-02-23 01:58:32 +0300

    Ждуъ @artemetra

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from bot_token import TOKEN
+from aiogram.types.input_file import InputFile
+
+from PIL import Image
+import os
 
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Send sticker and properly resized image for emoji will be returned")
+    await message.reply("Send sticker and a properly resized image for emoji will be returned")
 
 @dp.message_handler(content_types=["sticker"])
 async def echo(message: types.Message):
-    """Downloads sticker as an image to /sticker folder"""
-    await message.sticker.download()
+    sticker_path = await message.sticker.download()
+
+    image = Image.open(sticker_path.name)
+
+    # Waiting for Artemetra
+    
+    await message.answer_document(InputFile(sticker_path))
+    os.remove(sticker_path)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
